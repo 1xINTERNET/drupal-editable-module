aac6a73b8c7d8d547c7546ed16e05ca080f3d8c0
Martin Spencer <m.spencer@1xinternet.de>
2018-09-23 12:56:16 +0200

    add redux-json-api actions to library exports

--- js/src/index.js
+++ js/src/index.js
@@ -7,5 +7,12 @@ export { default as set } from "immutable-set";
 export { connect } from "react-redux";
 export { css } from "emotion";
 export { createSelector } from "reselect";
+export {
+  createResource,
+  readEndpoint,
+  updateResource,
+  deleteResource,
+  hydrateStore
+} from "redux-json-api";
 export { DataSet, Query, EditableEntity } from "./components";
 export { getQueryFromEntityReference, getQueryFromRIO } from "./normalizers";
