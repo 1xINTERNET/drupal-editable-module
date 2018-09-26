3c1e1b95b5a577c581c382a27e360b528684cb2d
Martin Spencer <m.spencer@1xinternet.de>
2018-09-26 14:28:57 +0200

    expose react-redux Provider

--- js/src/index.js
+++ js/src/index.js
@@ -14,7 +14,7 @@ export const store = createStore();
 // Alias and export other utility functions.
 export { default as get } from "lodash.get";
 export { default as set } from "immutable-set";
-export { connect } from "react-redux";
+export { connect, Provider } from "react-redux";
 export { css } from "emotion";
 export { createSelector } from "reselect";
 export {
