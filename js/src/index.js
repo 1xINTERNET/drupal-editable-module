53220a7a0d0e225adc0268b474a8a21f1e1e8b15
Martin Spencer <m.spencer@1xinternet.de>
2018-09-26 14:28:30 +0200

    better polyfills

--- js/src/index.js
+++ js/src/index.js
@@ -1,17 +1,14 @@
-// These imports will be exposed by webpack's expose-loader
-/* eslint-disable no-unused-vars */
-import * as React from "react";
-import ReactDOM from "react-dom";
-/* eslint-enable no-unused-vars */
+import "./polyfills";
 
-import { createStore } from "./utils";
-
-// Only import the prop-types package if this is a development build.
-if (process.env.NODE_ENV === "development") {
-  require("prop-types");
-}
+/* eslint-disable import/no-unresolved */
+import "expose-loader?React!react";
+import "expose-loader?ReactDOM!react-dom";
+import "expose-loader?PropTypes!prop-types";
+/* eslint-enable import/no-unresolved */
 
 // Create and export the shared redux store.
+import { createStore } from "./utils";
+
 export const store = createStore();
 
 // Alias and export other utility functions.
