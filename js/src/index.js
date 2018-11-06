93b492d800b6925d8bc3fe7afb78d03aae69ca5d
Martin Spencer <m.spencer@1xinternet.de>
2018-11-06 13:30:28 +0100

    Use data injected into drupalSettings for hydration

--- js/src/index.js
+++ js/src/index.js
@@ -1,21 +1,26 @@
 import { EditableRegistry } from "./editableRegistry";
+import { hydrateStore } from "@drupal-editable/core";
 
 (($, window, Drupal) => {
   // Instantiate the registry, this will be used by other modules to attach
   // their own middlewares, reducers etc.
-  window.Editable_Registry = new EditableRegistry();
+  const registry = (window.Editable_Registry = new EditableRegistry());
   const domain = `${window.location.protocol}//${window.location.host}`;
 
   Drupal.behaviors.editable_core_init = {
-    attach(context) {
+    async attach(context, { editable }) {
       // Only do this on the root doc ready call.
       if (context !== document) {
         return;
       }
 
-      window.Editable_Registry.initialize({
+      await registry.initialize({
         apiEndpoint: `${domain}/jsonapi`,
-        csrfTokenEndpoint: `${domain}/session/token`
+        csrfTokenEndpoint: `${domain}/session/token`,
+        hydrationData:
+          editable &&
+          editable.entities &&
+          Object.keys(editable.entities).map(k => editable.entities[k].data)
       });
     }
   };
